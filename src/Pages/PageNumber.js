3524f54ac0a79a5dd18e8210d5034b9c38e49d23
Yauheni_Korolkov <yauheni_karalkou@senla.eu>
2021-11-19 13:36:21 +0300

    add logic NUMBER page

--- src/Pages/PageNumber.js
+++ src/Pages/PageNumber.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function PageNamber() {
+    let [data, setData] = useState("");
+    
+    let [inputVal, setInputVal] = useState(1)
+
+    let changeinput = (event) => {
+        setInputVal(event.target.value)
+    }
+    
+    const addNumber = () => {        
+        setData(inputVal)
+        fetch(`http://numbersapi.com/${inputVal}/math`)
+        .then(response => response.text()
+        .then(text => setData(text)
+        )); 
+    }
+    
+
+
     return (
+        
         <div className="info">
             <h1>NUMBER</h1>
-            <p>Please input Number</p>
-            <input type="number"/>
-            <button>OK</button>
+            <p>Please input number</p>
+            <p>{data}</p>
+            <input type="number" value={inputVal} onChange={changeinput}/>
+            <button onClick={addNumber}>OK</button>
         </div>
     )
 }
