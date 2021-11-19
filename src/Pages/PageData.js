a241a9b678e1e7c4b93e10a3f0c3b2c938eb9343
Yauheni_Korolkov <yauheni_karalkou@senla.eu>
2021-11-19 13:58:12 +0300

    add logic DATE page

--- src/Pages/PageData.js
+++ src/Pages/PageData.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function PageData() {
+export default function PageYear() {
+    let [res, setRes] = useState("")
+    let [mounth, setMounth] = useState("1");
+    let [day, setDay] = useState("1")    
+    let [inputValMounth, setinputValMounth] = useState(1)
+    let [inputValDay, setinputValDay] = useState(1)
+
+    let changeinputMouth = (event) => {
+        setinputValMounth(event.target.value)
+    }    
+    let changeinputDay = (event) => {
+        setinputValDay(event.target.value)
+    } 
+    const createDate = () => {        
+        setMounth(inputValMounth)
+        setDay(inputValDay)
+
+        if (mounth <= 12 && day <= 12){
+
+            fetch(`http://numbersapi.com/${mounth}/${day}/date`)
+            .then(response => response.text()
+            .then(text => setRes(text)
+            ));
+    
+        }
+    }
     return (
-    <div className="info">
-        <h1>DATA</h1>
-        <p>Please input Data</p>
-        <input type="number"/>
-        <input type="number"/>
-        <button>OK</button>
-
-    </div>
+        <div className="info">
+            <h1>YEAR</h1>
+            <p>Please input Year</p>
+            <p>{res}</p>
+            <input type="number" value={inputValMounth} onChange={changeinputMouth}/>
+            <input type="number" value={inputValDay} onChange={changeinputDay}/>
+            <button onClick={createDate}>OK</button>
+        </div>
     )
 }
-
 
 
